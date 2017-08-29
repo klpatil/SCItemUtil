381f0dbc6b7d9eb29c846f07a20b7b7c00817cfe
klpatil <klpatil@users.noreply.github.com>
2017-08-29 23:33:58 +0530

    First commit

---
+++ src/ItemUtil.aspx
@@ -0,0 +1,127 @@
+﻿<%@ page language="C#" autoeventwireup="true" CodeBehind="ItemUtil.aspx.cs" inherits="sc82basics.sitecore.admin.ItemUtil" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Item Utility</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style type="text/css">
+        /*http://getbootstrap.com/examples/navbar-fixed-top/navbar-fixed-top.css*/
+        body {
+            min-height: 2000px;
+            padding-top: 70px;
+        }
+    </style>
+
+
+    
+</head>
+<body>
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">HI</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Item Util</a></li>
+                </ul>
+            </div>
+            <!--/.nav-collapse -->
+        </div>
+    </nav>
+
+    <div class="container">
+        <form id="form1" runat="server" class="form-horizontal" role="form" data-toggle="validator">
+            <div class="form-group">
+                <label for="txtStartPath" class="col-sm-2 control-label">Start Path</label>
+                <div class="col-sm-10">
+                    <asp:TextBox ID="txtStartPath" runat="server" placeholder="Path" CssClass="form-control" required />
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="txtSourceTemplatePath" class="col-sm-2 control-label">Source Template Path</label>
+                <div class="col-sm-10">
+                    <asp:TextBox ID="txtSourceTemplatePath" runat="server" placeholder="Source Template Path" CssClass="form-control" required />
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="txtTargetTemplatePath" class="col-sm-2 control-label">Target Template Path</label>
+                <div class="col-sm-10">
+                    <asp:TextBox ID="txtTargetTemplatePath" runat="server" placeholder="Target Template Path" CssClass="form-control" required/>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                    <div class="checkbox">
+                        <asp:CheckBox ID="chkAllChildrens" runat="server" Text="All Childrens?" />
+                    </div>
+                    <div class="checkbox">
+                        <asp:CheckBox ID="chkFastandFurious" runat="server" Text="Fast and Furious?" ToolTip="Would you like to disable Security/Proxy/DatabaseCache/Event/Indexing - Fastest way. But needs some manual steps once done" />
+                    </div>
+                </div>                
+            </div>           
+            <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                    <asp:Button ID="btnStartProcessing" runat="server" Text="Start Processing" CssClass="btn btn-default" UseSubmitBehavior="true"
+                         OnClick="btnStartProcessing_Click" />                    
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="txtProcessResult" class="col-sm-2 control-label">Process Result</label>
+                <div class="col-sm-10">
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            <asp:TextBox ID="txtProcessResult" runat="server" ReadOnly="true" CssClass="form-control"
+                                TextMode="MultiLine" Rows="20" Columns="50" Enabled="false" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+
+
+    <%--SCRIPTS--%>
+    <script
+        src="https://code.jquery.com/jquery-3.2.1.min.js"
+        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.9/validator.min.js"></script>
+    <script type="text/javascript">       
+        //http://1000hz.github.io/bootstrap-validator/
+        $('#form1').validator().on('submit', function (e) {
+            if (e.isDefaultPrevented()) {
+                // handle the invalid form...
+            } else {
+                return confirm('Are you sure?');
+            }
+        })          
+    </script>
+</body>
+</html>
---
+++ src/ItemUtil.aspx.cs
@@ -0,0 +1,192 @@
+﻿using SCBasics.Kernel;
+using Sitecore.ContentSearch.Maintenance;
+using Sitecore.Data;
+using Sitecore.Data.Events;
+using Sitecore.Data.Items;
+using Sitecore.Data.Proxies;
+using Sitecore.SecurityModel;
+using Sitecore.sitecore.admin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace sc82basics.sitecore.admin
+{
+    
+    public partial class ItemUtil : AdminPage
+    {
+        int successItemsCount;
+        int ignoreItemsCount;
+
+        protected override void OnInit(EventArgs e)
+        {
+            base.CheckSecurity(true); //Required!
+            base.OnInit(e);
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {            
+            
+            Server.ScriptTimeout = int.MaxValue;
+        }
+
+        protected void btnStartProcessing_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (ValidateInputs())
+                {
+                    ShowMessage("Please make sure all required inputs are provided", "Once done, please try again");
+                }
+                else
+                {
+                    // Start processing
+                    Database database = Sitecore.Configuration.Factory.GetDatabase("master");
+
+                    if (database != null)
+                    {
+                        Item startItem = database.GetItem(txtStartPath.Text.Trim());
+
+                        if (startItem != null)
+                        {
+                            Item sourceTemplateItem = database.GetItem(txtSourceTemplatePath.Text.Trim());
+                            Item targetTemplateItem = database.GetItem(txtTargetTemplatePath.Text.Trim());
+
+                            if (sourceTemplateItem != null || targetTemplateItem != null)
+                            {
+                                
+
+                                StringBuilder sBuilder = new StringBuilder(string.Empty);
+                                DateTime dtStart = DateTime.Now;
+
+                                bool result = ChangeTemplate(startItem, sourceTemplateItem, targetTemplateItem, sBuilder, chkAllChildrens.Checked);                                
+
+                                DateTime dtEnd = DateTime.Now;
+                                TimeSpan ts = dtEnd - dtStart;
+                                sBuilder.AppendLine("===================SUMMARY=========================");
+                                sBuilder.AppendLine("Time Taken (MS): " + ts.TotalMilliseconds.ToString());
+                                sBuilder.AppendLine("Operation completed successfuly. Total Items found " + (successItemsCount + ignoreItemsCount) + ".");
+                                sBuilder.AppendLine("Successfully coverted items Count: " + successItemsCount);
+                                sBuilder.AppendLine("Ignored items Count: " + ignoreItemsCount);
+                                if (!string.IsNullOrEmpty(sBuilder.ToString()))
+                                {
+                                    txtProcessResult.Text =  sBuilder.ToString();
+                                    // Log full details
+                                    Sitecore.Diagnostics.Log.Info("Processing completed : " + sBuilder.ToString(), this);
+                                }
+                               
+                            }
+                            else
+                            {
+                                ShowMessage("Source/Target Template item not found", txtSourceTemplatePath.Text + "/" + txtTargetTemplatePath.Text);
+                            }
+                        }
+                        else
+                        {
+                            ShowMessage("Start Path item not found", txtStartPath.Text);                            
+                        }
+                    }
+                    else
+                    {
+                        ShowMessage("Database not found", "master");
+                    }
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Sitecore.Diagnostics.Log.Error("Start processing click", ex, this);
+                ShowMessage("Something went wrong while processing. Please check logs for detaild information", ex.Message);
+            }
+        }
+        
+        private bool ChangeTemplate(Item startItem, Item sourceTemplateItem, Item targetTemplateItem, StringBuilder sBuilder, bool @checked)
+        {
+            bool result = false;
+            if (chkFastandFurious.Checked)
+            {
+                // https://blog.horizontalintegration.com/2016/02/12/disablers-disablers-disablers-disablers-a-lesson-in-mass-sitecore-updates/
+                using (new SecurityDisabler())
+                using (new ProxyDisabler())
+                using (new DatabaseCacheDisabler())
+                using (new EventDisabler())
+                using (new BulkUpdateContext())
+                {
+                    try
+                    {
+                        sBuilder.AppendLine("ChangeTemplate Started In Fast And Furious way");
+                        // https://blog.krusen.dk/disable-indexing-temporarily-in-sitecore-7/
+                        IndexCustodian.PauseIndexing();
+                        ChangeTemplateExecute(startItem, sourceTemplateItem, targetTemplateItem, sBuilder);
+
+                        if (@checked)
+                        {
+                            foreach (Item item in startItem.Children)
+                            {
+                                ChangeTemplateExecute(item, sourceTemplateItem, targetTemplateItem, sBuilder);
+                            }
+                        }
+                    }
+                    finally
+                    {
+                        IndexCustodian.ResumeIndexing();
+                    }
+                }
+            }
+            else
+            {
+                sBuilder.AppendLine("ChangeTemplate Started In Slow and Steady way");
+                // Process Main Item
+                ChangeTemplateExecute(startItem, sourceTemplateItem, targetTemplateItem, sBuilder);
+
+                if (@checked)
+                {
+                    foreach (Item item in startItem.Children)
+                    {
+                        ChangeTemplateExecute(item, sourceTemplateItem, targetTemplateItem, sBuilder);
+                    }
+                }
+            }
+            
+            result = true;
+            return result;           
+        }
+
+        private void ChangeTemplateExecute(Item item, Item sourceTemplateItem, Item targetTemplateItem, StringBuilder sBuilder)
+        {
+            if (item.TemplateID == sourceTemplateItem.ID)
+            {
+                item.ChangeTemplate(targetTemplateItem);
+                sBuilder.AppendFormat("* Template changed successfully for Item : {0} to Template : {1}.", item.Paths.Path,
+                    targetTemplateItem.Paths.Path);
+                sBuilder.AppendLine();
+                successItemsCount++;
+            }
+            else
+            {
+                // Skipped as tempalte no match
+                sBuilder.AppendFormat("* Template change skipped for Item : {0} to Template : {1} as No Template Match found.", item.Paths.Path,
+                    targetTemplateItem.Paths.Path);
+                sBuilder.AppendLine();
+                ignoreItemsCount++;
+            }
+        }
+
+        private bool ValidateInputs()
+        {
+            return string.IsNullOrWhiteSpace(txtStartPath.Text) || string.IsNullOrWhiteSpace(txtSourceTemplatePath.Text) || 
+                string.IsNullOrWhiteSpace(txtTargetTemplatePath.Text);
+        }
+
+        private void ShowMessage(string message, string detailedmessage)
+        {
+            txtProcessResult.Text = string.Format("{0} - {1}", message, detailedmessage);
+        }
+    }
+}
